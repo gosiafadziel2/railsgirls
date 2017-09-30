8e11f1b7cb0cbb9ce23261f3e4a482ffd8337a2a
Michał Podkowa <cyrioo@gmail.com>
2017-09-30 12:27:28 +0200

    file upload added

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,4 @@
 class Idea < ApplicationRecord
-  validates :name, :description, :picture, presence: true #or {presence: true}, b/c it's last param in func invocation
+  mount_uploader :picture, PictureUploader
+  validates :name, :description, :picture, presence: true #or {presence: true}, b/c it's last param in func invocationx
 end
